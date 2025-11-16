4bd2726bde3e1241867be76fec83bce6a46bc279
yeeJin227 <dpwls99600@gmail.com>
2025-11-16 19:13:54 +0900

    refactor: productUuid DB 저장 방식 @PrePersist로 변경

--- src/main/java/com/back/domain/product/product/entity/Product.java
+++ src/main/java/com/back/domain/product/product/entity/Product.java
@@ -28,8 +28,14 @@ public class Product extends BaseEntity {
     private User user; // 작가 FK
 
     @Column(unique = true, nullable = false, updatable = false)
-    @Builder.Default
-    private UUID productUuid = UUID.randomUUID(); // 상품 식별 번호 (uuid)
+    private UUID productUuid; // 상품 식별 번호 (uuid)
+
+    @PrePersist
+    private void generateUuid() {
+        if (productUuid == null) {
+            productUuid = UUID.randomUUID();
+        }
+    }
 
     @Column(nullable = false)
     private String name; // 상품명
